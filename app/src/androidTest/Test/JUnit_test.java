60265e55f063437126aff7815a88571c5a1720c6
kmklein <froggy123>
2016-04-19 19:52:15 -0700

    No

---
+++ app/src/androidTest/Test/JUnit_test.java
@@ -0,0 +1,32 @@
+package com.example.kmklein.lab3.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.kmklein.lab3.MainActivity;
+import com.example.kmklein.lab3.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by kmklein on 4/13/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity mainActivity;
+
+    public JUnit_test() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = getActivity();
+    }
+
+    public void test_first() {
+        mainActivity = getActivity();
+
+        assertEquals(0, sum(0, 0));
+    }
+}
---
+++ app/src/main/java/com/example/kmklein/dimsum/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.kmklein.dimsum;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText x;
+    private EditText y;
+    private Button btnAdd;
+    private TextView answer;
+    int num1, num2, sum;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        x = (EditText)findViewById(R.id.editText);
+        y = (EditText)findViewById(R.id.editText2);
+        btnAdd = (Button)findViewById(R.id.button);
+        answer = (TextView)findViewById(R.id.textView);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)  {
+                if (x.getText().toString() == "") { num1 = 0; }
+                else { num1 = Integer.parseInt(x.getText().toString()); }
+                if (x.getText().toString() == "") { num2 = 0; }
+                else { num2 = Integer.parseInt(y.getText().toString()); }
+                sum = sum(num1, num2);
+                answer.setText(Integer.toString(sum));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    int sum(int a, int b)
+    {
+        return a+b;
+    }
+}
